670a953ba15b0c43fc58d4574b92da8ed2a5ae24
hayaman3 <kevin.haya@yahoo.com>
2023-10-26 18:43:57 +0800

    removed nextjs boiler plate

---
+++ src/app/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+
+export type HeaderProps = {
+	//no props
+};
+
+const Header: FunctionComponent<HeaderProps> = ({}) => {
+	return (
+		<header>
+			<span>Header</span>
+		</header>
+	);
+};
+
+export default Header;
