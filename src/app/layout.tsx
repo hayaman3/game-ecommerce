111169b32710869be2c2fc1d7f19307b1008655c
hayaman3 <kevin.haya@yahoo.com>
2023-11-02 20:32:41 +0800

    changed folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Header from './Header/Header';
+// import Header from './Header/Header';
+import Header from '@components/Header/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`bg-gray-900 ${inter.className}`}>
+			<body className={`bg-[rgba(19,19,19,1)] ${inter.className}`}>
 				<Header />
 				{children}
 			</body>
