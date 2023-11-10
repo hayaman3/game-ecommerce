1973e36161a145461b21550a179ea2670e2d6079
hayaman3 <kevin.haya@yahoo.com>
2023-11-10 21:42:32 +0800

    removed unused code

--- src/app/Cart/Cart.tsx
+++ src/app/Cart/Cart.tsx
@@ -1,15 +1,5 @@
 import React, { FunctionComponent } from "react";
 
-export type CartProps = {
-  //no props
-};
-
-const Cart: FunctionComponent<CartProps> = ({}) => {
-  return <>Cart</>;
-};
-
-export default Cart;
-
 import {
   Drawer,
   Button,
@@ -28,44 +18,6 @@ export function DrawerPlacement() {
       <div className="flex flex-wrap gap-4">
         <Button onClick={openDrawerRight}>Open Drawer Right</Button>
       </div>
-      <Drawer
-        placement="top"
-        open={openTop}
-        onClose={closeDrawerTop}
-        className="p-4"
-      >
-        <div className="mb-6 flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray">
-            Material Tailwind
-          </Typography>
-          <IconButton variant="text" color="blue-gray" onClick={closeDrawerTop}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </IconButton>
-        </div>
-        <Typography color="gray" className="mb-8 pr-4 font-normal">
-          Material Tailwind features multiple React and HTML components, all
-          written with Tailwind CSS classes and Material Design guidelines.
-        </Typography>
-        <div className="flex gap-2">
-          <Button size="sm" variant="outlined">
-            Documentation
-          </Button>
-          <Button size="sm">Get Started</Button>
-        </div>
-      </Drawer>
       <Drawer
         placement="right"
         open={openRight}
@@ -108,90 +60,16 @@ export function DrawerPlacement() {
           <Button size="sm">Get Started</Button>
         </div>
       </Drawer>
-      <Drawer
-        placement="bottom"
-        open={openBottom}
-        onClose={closeDrawerBottom}
-        className="p-4"
-      >
-        <div className="mb-6 flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray">
-            Material Tailwind
-          </Typography>
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            onClick={closeDrawerBottom}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </IconButton>
-        </div>
-        <Typography color="gray" className="mb-8 pr-4 font-normal">
-          Material Tailwind features multiple React and HTML components, all
-          written with Tailwind CSS classes and Material Design guidelines.
-        </Typography>
-        <div className="flex gap-2">
-          <Button size="sm" variant="outlined">
-            Documentation
-          </Button>
-          <Button size="sm">Get Started</Button>
-        </div>
-      </Drawer>
-      <Drawer
-        placement="left"
-        open={openLeft}
-        onClose={closeDrawerLeft}
-        className="p-4"
-      >
-        <div className="mb-6 flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray">
-            Material Tailwind
-          </Typography>
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            onClick={closeDrawerLeft}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </IconButton>
-        </div>
-        <Typography color="gray" className="mb-8 pr-4 font-normal">
-          Material Tailwind features multiple React and HTML components, all
-          written with Tailwind CSS classes and Material Design guidelines.
-        </Typography>
-        <div className="flex gap-2">
-          <Button size="sm" variant="outlined">
-            Documentation
-          </Button>
-          <Button size="sm">Get Started</Button>
-        </div>
-      </Drawer>
     </React.Fragment>
   );
 }
+
+export type CartProps = {
+  //no props
+};
+
+const Cart: FunctionComponent<CartProps> = ({}) => {
+  return <>Cart</>;
+};
+
+export default Cart;
