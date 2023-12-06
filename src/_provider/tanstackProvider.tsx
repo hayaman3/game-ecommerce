98e87360b01d902ceb884c1da9b1cd742f3ed4a6
hayaman3 <kevin.haya@yahoo.com>
2023-12-06 23:46:51 +0800

    init tanstackquery

---
+++ src/_provider/tanstackProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { FunctionComponent, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export type tanstackProviderProps = {
+  children: ReactNode;
+};
+
+const tanstackProvider: FunctionComponent<tanstackProviderProps> = ({
+  children,
+}) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default tanstackProvider;
