63e985b83213d53686a69b02780042d65b5f6740
hayaman3 <kevin.haya@yahoo.com>
2024-02-22 16:48:24 +0800

    added Button Component

--- src/app/Shop/_components/_card/ProductDetail.tsx
+++ src/app/Shop/_components/_card/ProductDetail.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+// import Button from "./Button";
 
 export type ProductDetailProps = {
   price: string;
@@ -11,8 +12,14 @@ const ProductDetail: FunctionComponent<ProductDetailProps> = ({
 }) => {
   return (
     <div className="absolute bottom-0 flex h-1/4 w-full bg-[#333333] bg-opacity-50 bg-clip-padding p-4 text-white backdrop-blur-md backdrop-filter">
-      <span className="text-sm">{price}</span>
-      <span className="text-sm">{title}</span>
+      <div className="flex">
+        <span className="text-sm">{title}</span>
+        <span className="text-sm">{price}</span>
+      </div>
+      <div className="flex">
+        {/* <Button />
+        <Button /> */}
+      </div>
     </div>
   );
 };
