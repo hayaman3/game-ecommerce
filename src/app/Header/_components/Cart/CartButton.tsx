01376830643ad25d7d1ab4a05372f500b2167e8a
hayaman3 <kevin.haya@yahoo.com>
2024-03-11 13:31:16 +0800

    added context api to cartButton.tsx

--- src/app/Header/_components/Cart/CartButton.tsx
+++ src/app/Header/_components/Cart/CartButton.tsx
@@ -1,27 +1,26 @@
 "use client";
 import React, { FunctionComponent, useState } from "react";
 import CartIcon from "../../_icons/CartIcon";
+import { useQuantity } from "@/_provider/QuantityProvider";
 
 export type CartButtonProps = {
-  cartQuantity?: number | null;
+  cartQuantity: number;
   toggleCartDrawer: () => void;
 };
 
 const CartButton: FunctionComponent<CartButtonProps> = ({
-  cartQuantity,
   toggleCartDrawer,
 }) => {
+  const { cartQuantity } = useQuantity();
   return (
     <button className="relative" onClick={toggleCartDrawer}>
       <CartIcon />
-      {cartQuantity ? (
+      {cartQuantity > 0 && (
         <span className="absolute -right-[6px] bottom-0 flex h-4 w-4 content-center items-center justify-center rounded-full border-2 border-dark-300 bg-white p-2">
           <span className="text-center text-xs font-bold text-dark-300">
             {cartQuantity}
           </span>
         </span>
-      ) : (
-        ""
       )}
     </button>
   );
