14218e56b3675bdc51178dc8075617e34843f690
hayaman3 <kevin.haya@yahoo.com>
2024-03-25 20:09:50 +0800

    added Image url to config

--- src/app/Products/Products.tsx
+++ src/app/Products/Products.tsx
@@ -4,24 +4,6 @@ export type ProductsProps = {
   //no props
 };
 
-async function getProducts() {
-  const response = await fetch("https://dummyjson.com/products"); // fetch the products
-  const data = await response.json(); // convert the response to json
-  // setProducts(data.products); // set the products in the state to the products we fetched
-}
-
-const RawgAPI = async () => {
-  const response = await fetch(
-    // `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&dates=2019-01-01,2019-12-31&ordering=-added`
-    // `https://api.rawg.io/api/platforms?key=${process.env.RAWG_API_KEY}`
-    `https://api.rawg.io/api/stores?key=${process.env.RAWG_API_KEY}`,
-  );
-  if (response.ok) {
-    return response.json();
-  }
-  throw new Error("");
-};
-
 const Products: FunctionComponent<ProductsProps> = ({}) => {
   return <>Products</>;
 };
--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -1,18 +1,19 @@
 "use client";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import Image from "next/image";
 import axios from "axios";
 
-interface Data {
-  userId: number;
+type Data = {
   id: number;
-  title: string;
-  body: string;
-}
+  name: string;
+  background_image: string;
+  rating: number;
+};
 
 const queryClient = new QueryClient();
 
@@ -27,19 +28,37 @@ const Test: FunctionComponent = () => {
 };
 
 const Page: FunctionComponent = () => {
+  const [trial, setTrial] = useState<string>("");
   const { data } = useQuery({
     queryKey: ["rawg"],
 
     queryFn: async () => {
       const { data } = await axios.get("/api/page_two");
       console.log(data);
-      return data as Data;
+      return data as Data[];
     },
   });
+
+  useEffect(() => {
+    if (!data) return;
+    setTrial(data[0].background_image);
+  }, [data]);
 
   return (
     <main className="mt-[100px] w-[300px] p-20">
       <div>{JSON.stringify(data)}</div>
+      {data &&
+        data.map(({ id, name, background_image, rating }) => (
+          <Image
+            key={id}
+            src={background_image}
+            width={500}
+            height={500}
+            alt={name}
+            placeholder="blur"
+            // className="transition"
+          />
+        ))}
     </main>
   );
 };
