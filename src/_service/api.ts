3f5456bf5e5297ed9d7047beea28594f622ffe66
hayaman3 <kevin.haya@yahoo.com>
2024-04-05 23:18:28 +0800

    added utils function for api

--- src/_service/api.ts
+++ src/_service/api.ts
@@ -1,4 +1,34 @@
 import axios from "axios";
+import { 
+  useQuery,
+  useMutation, 
+  useQueryClient
+ } from "@tanstack/react-query"
+
+
+export const useApiGet = (key, fn, options) => useQuery({
+    queryKey: key,
+    queryFn: fn,
+    ...options
+})
+
+export const useApiSend = (fn, success, error, invalidateKey, options) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: fn,
+        onSuccess: (data) => {
+            invalidateKey &&
+                invalidateKey.forEach((key) => {
+                    queryClient.invalidateQueries(key);
+                });
+            success && success(data);
+        },
+        onError: error,
+        retry: 2,
+        ...options, 
+    });
+};
 
 const API_BASE_URL = "https://api.rawg.io/api/game";
 
@@ -10,3 +40,38 @@ export const getProducts = async () => {
   const response = await api.get("/products");
   return response.data;
 };
+
+export const getFeeds = () =>
+    request({
+        url: `/feed`,
+        method: "GET",
+    })
+
+const {
+        data,
+        isLoading,
+        error,
+        isError,
+        isLoadingError,
+        refetch } = useApiGet(
+            ["feeds"],
+            getFeeds,
+            {
+                enabled: true,
+                refetchOnWindowFocus: true,
+                retry: 1
+            }
+        );
+
+
+    if (isLoading)
+        return <ScreenLoader />;
+
+    if (isError || isLoadingError)
+        return <ErrorPage
+            error={error?.message}
+return (
+  <div>
+    {data?.map((item,index))=> <PostCard item={item} key={index}/>}
+  </div>
+)
--- src/app/Products/Products.tsx
+++ src/app/Products/Products.tsx
@@ -1,10 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React, { FunctionComponent } from "react";
 
 export type ProductsProps = {
   //no props
 };
 
+interface Data {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 const Products: FunctionComponent<ProductsProps> = ({}) => {
+  const { data } = useQuery({
+    queryKey: ["rawg"],
+
+    queryFn: async () => {
+      const { data } = await axios.get("/api/page_two");
+      console.log(data);
+      return data as Data;
+    },
+  });
+  
   return <>Products</>;
 };
 
